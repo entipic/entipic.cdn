971a1907c74d0321dfc079913277001f9f6037aa
Dumitru C <canteadumitru@gmail.com>
2020-02-06 16:03:06 +0200

    set content-length

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,70 @@
+require("dotenv").config();
 
-require('dotenv').config();
-
-import * as express from 'express';
-import { logger } from './logger';
-import imageHandler from './image-handler';
-import { NextFunction, Response, Request } from 'express';
-import { initData } from './data';
-const cors = require('cors');
+import * as express from "express";
+import { logger } from "./logger";
+import imageHandler from "./image-handler";
+import { NextFunction, Response, Request } from "express";
+import { initData } from "./data";
+const cors = require("cors");
 const PORT = process.env.PORT;
 
 async function start() {
-    const server = express();
+  const server = express();
 
-    server.disable('x-powered-by');
+  server.disable("x-powered-by");
 
-    server.use(cors());
+  server.use(cors());
 
-    const sizes = ['x', 'a', 'b', 'c', 'd', 'e', 'f'];
+  const sizes = ["x", "a", "b", "c", "d", "e", "f"];
 
-    server.get(`/picture/:size(${sizes.join('|')})/:id.:format(jpg|webp)`, imageHandler);
-    server.get(`/picture/:id.:format(jpg|webp)`, imageHandler);
+  server.get(
+    `/picture/:size(${sizes.join("|")})/:id.:format(jpg|webp)`,
+    imageHandler
+  );
+  server.get(`/picture/:id.:format(jpg|webp)`, imageHandler);
 
-    server.get(`/:lang([a-z]{2})-:country([a-z]{2})/:size(${sizes.join('|')})/:name.:format(jpg|webp)`, imageHandler);
-    server.get(`/:lang([a-z]{2})/:size(${sizes.join('|')})/:name.:format(jpg|webp)`, imageHandler);
-    server.get(`/:lang([a-z]{2})-:country([a-z]{2})/:name.:format(jpg|webp)`, imageHandler);
-    server.get(`/:size(${sizes.join('|')})/:name.:format(jpg|webp)`, imageHandler);
-    server.get(`/:lang([a-z]{2})/:name.:format(jpg|webp)`, imageHandler);
-    server.get(`/:name.:format(jpg|webp)`, imageHandler);
+  server.get(
+    `/:lang([a-z]{2})-:country([a-z]{2})/:size(${sizes.join(
+      "|"
+    )})/:name.:format(jpg|webp)`,
+    imageHandler
+  );
+  server.get(
+    `/:lang([a-z]{2})/:size(${sizes.join("|")})/:name.:format(jpg|webp)`,
+    imageHandler
+  );
+  server.get(
+    `/:lang([a-z]{2})-:country([a-z]{2})/:name.:format(jpg|webp)`,
+    imageHandler
+  );
+  server.get(
+    `/:size(${sizes.join("|")})/:name.:format(jpg|webp)`,
+    imageHandler
+  );
+  server.get(`/:lang([a-z]{2})/:name.:format(jpg|webp)`, imageHandler);
+  server.get(`/:name.:format(jpg|webp)`, imageHandler);
 
-    server.use((_req, res) => res.sendStatus(404).end());
+  server.use((_req, res) => res.sendStatus(404).end());
 
-    server.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-        logger.error(err.message, err);
-        res.status(500).send(err.message);
-    });
+  server.use(
+    (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+      logger.error(err.message, err);
+      res.status(500).send(err.message);
+    }
+  );
 
-    await initData();
-    await server.listen(PORT);
+  await initData();
+  server.listen(PORT);
 }
 
-process.on('unhandledRejection', function (error: Error) {
-    logger.error('unhandledRejection: ' + error.message, error);
+process.on("unhandledRejection", function(error: Error) {
+  logger.error("unhandledRejection: " + error.message, error);
 });
 
-process.on('uncaughtException', function (error: Error) {
-    logger.error('uncaughtException: ' + error.message, error);
+process.on("uncaughtException", function(error: Error) {
+  logger.error("uncaughtException: " + error.message, error);
 });
 
 start()
-    .then(() => logger.warn(`Listening at ${PORT}`))
-    .catch(e => {
-        logger.error(e);
-    });
+  .then(() => logger.warn(`Listening at ${PORT}`))
+  .catch(logger.error);
--- src/handle-image-id.ts
+++ src/handle-image-id.ts
@@ -1,46 +1,55 @@
 import { Response, NextFunction } from "express";
-import * as sharp from 'sharp';
-import * as got from 'got';
+import * as sharp from "sharp";
+import * as got from "got";
 import { Duplex } from "stream";
-import { PictureSizeName, PictureHelper } from '@entipic/domain';
+import { PictureSizeName, PictureHelper } from "@entipic/domain";
 import { PictureFormat } from "./types";
 
+export function handleImageId(
+  id: string,
+  format: PictureFormat,
+  size: PictureSizeName,
+  res: Response,
+  next: NextFunction
+) {
+  const originalFormat = "jpg";
+  const masterSizeName = "f";
+  const url = PictureHelper.formatS3Url(id);
 
-export function handleImageId(id: string, format: PictureFormat, size: PictureSizeName, res: Response, next: NextFunction) {
+  const stream = got
+    .stream(url, { timeout: 3000 })
+    // .on("end")
+    .on("error", error => next(error));
 
-    const originalFormat = 'jpg';
-    const masterSizeName = 'f';
-    const url = PictureHelper.formatS3Url(id);
+  if (format === originalFormat && size === masterSizeName) {
+    return sendImage(stream, res, format);
+  }
+  let instance = sharp();
+  if (format !== originalFormat) {
+    instance = instance.toFormat(format, { quality: 100 });
+  }
+  if (size !== masterSizeName) {
+    const newSize = PictureHelper.getPictureSize(size);
+    instance = instance.resize(newSize, newSize, { kernel: "cubic" });
+  }
 
-    const stream = got
-        .stream(url, { timeout: 3000 })
-        .on('error', error => next(error));
+  instance.once("readable", () => {
+    res.setHeader("content-length", instance.readableLength);
+  });
 
-    if (format === originalFormat && size === masterSizeName) {
-        return sendImage(stream, res, format);
-    }
-    let instance = sharp();
-    if (format !== originalFormat) {
-        instance = instance.toFormat(format, { quality: 100 });
-    }
-    if (size !== masterSizeName) {
-        const newSize = PictureHelper.getPictureSize(size);
-        instance = instance.resize(newSize, newSize, { kernel: 'cubic' });
-    }
-
-    return sendImage(stream.pipe(instance), res, format);
+  return sendImage(stream.pipe(instance), res, format);
 }
 
 function sendImage(stream: Duplex, res: Response, format: PictureFormat) {
-    res.setHeader('content-type', getMime(format));
-    res.setHeader('cache-control', 'public, max-age=2592000'); // 30 days
+  res.setHeader("content-type", getMime(format));
+  res.setHeader("cache-control", "public,max-age=2592000"); // 30 days
 
-    stream.pipe(res);
+  stream.pipe(res);
 }
 
 function getMime(format: PictureFormat) {
-    if (format === 'webp') {
-        return 'image/webp';
-    }
-    return 'image/jpeg';
+  if (format === "webp") {
+    return "image/webp";
+  }
+  return "image/jpeg";
 }
